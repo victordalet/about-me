279b6b29eeaa1f0618eaebdab925341e351c6165
victordalet <victordalet@protonmail.com>
2022-01-02 21:55:04 +0100

    rajout de la class carte et de la carte macto

--- index/script.js
+++ index/script.js
@@ -16,6 +16,25 @@ setTimeout(()=> {
 },200);
 
 
+/**************************  CARDS *****************************/
+var card12 = document.querySelector(".card--12");
+
+card12.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card12.style.setProperty('--mouseX12', (e.clientX - ww) / 25);
+  card12.style.setProperty('--mouseY12', (e.clientY - wh) / 25);
+
+});
+
+card12.addEventListener('mouseleave', function (e) {
+
+  card12.style.setProperty('--mouseX12', 0);
+  card12.style.setProperty('--mouseY12', 0);
+
+});
+
 /**************************  ANIMATION *****************************/
 
 const img = document.querySelector("img");
