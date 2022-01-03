d981ea3955a00491b4f506091d1729f3f3a1cca0
victordalet <victordalet@protonmail.com>
2022-01-03 21:24:47 +0100

    rajout de l'animation de l'avion en papier (bug)

--- index/script.js
+++ index/script.js
@@ -139,3 +139,41 @@ TL1
 TL1.play()
 
 
+/*********************** SCROLL ****************************/
+
+const flightPath = {
+	cirviness : 1.25,
+	autoRotate : true,
+	values : [
+		{x:100,y:-20},
+		{x:300,y:10},
+		{x:500,y:100},
+		{x:750,y:-100},
+		{x:350,y:-50},
+		{x:600,x:100},
+		{x:800,y:0},
+		{x:window.innerWidth,y:-250}
+	]
+};
+
+
+const tween = new TimelineLite();
+
+tween.add(
+	TweenLite.to('.paper-plane',1,{
+		bezier : flightPath,
+		ease : Power1.easeInOut
+	})
+	);
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+	triggerElement : '.name',
+	duration:2000,
+	triggerHook: 0
+})
+
+.setTween(tween)
+.addIndicators()
+.setPin('.name')
+.addTo(controller);
