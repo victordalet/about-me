8d9c728b53a04310f2dc59dd79260c9c2d10555b
victordalet <victordalet@protonmail.com>
2022-01-03 19:25:36 +0100

    gestion des animations des cartes

--- index/script.js
+++ index/script.js
@@ -17,23 +17,114 @@ setTimeout(()=> {
 
 
 /**************************  CARDS *****************************/
-var card12 = document.querySelector(".card--12");
+var card1 = document.querySelector(".card--1");
 
-card12.addEventListener('mousemove', function (e) {
+card1.addEventListener('mousemove', function (e) {
 
   var wh = window.innerHeight / 2,
   ww = window.innerWidth / 2;
-  card12.style.setProperty('--mouseX12', (e.clientX - ww) / 25);
-  card12.style.setProperty('--mouseY12', (e.clientY - wh) / 25);
+  card1.style.setProperty('--mouseX1', (e.clientX - ww) / 25);
+  card1.style.setProperty('--mouseY1', (e.clientY - wh) / 25);
 
 });
 
-card12.addEventListener('mouseleave', function (e) {
+card1.addEventListener('mouseleave', function (e) {
 
-  card12.style.setProperty('--mouseX12', 0);
-  card12.style.setProperty('--mouseY12', 0);
+  card1.style.setProperty('--mouseX1', 0);
+  card1.style.setProperty('--mouseY1', 0);
 
 });
+/******************/
+var card2 = document.querySelector(".card--2");
+
+card2.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card2.style.setProperty('--mouseX2', (e.clientX - ww) / 25);
+  card2.style.setProperty('--mouseY2', (e.clientY - wh) / 25);
+
+});
+
+card2.addEventListener('mouseleave', function (e) {
+
+  card2.style.setProperty('--mouseX2', 0);
+  card2.style.setProperty('--mouseY2', 0);
+
+});
+/******************/
+var card3 = document.querySelector(".card--3");
+
+card3.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card3.style.setProperty('--mouseX3', (e.clientX - ww) / 25);
+  card3.style.setProperty('--mouseY3', (e.clientY - wh) / 25);
+
+});
+
+card3.addEventListener('mouseleave', function (e) {
+
+  card3.style.setProperty('--mouseX3', 0);
+  card3.style.setProperty('--mouseY3', 0);
+
+});
+/******************/
+var card4 = document.querySelector(".card--4");
+
+card4.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card4.style.setProperty('--mouseX4', (e.clientX - ww) / 25);
+  card4.style.setProperty('--mouseY4', (e.clientY - wh) / 25);
+
+});
+
+card4.addEventListener('mouseleave', function (e) {
+
+  card4.style.setProperty('--mouseX4', 0);
+  card4.style.setProperty('--mouseY4', 0);
+
+});
+/******************/
+var card5 = document.querySelector(".card--5");
+
+card5.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card5.style.setProperty('--mouseX5', (e.clientX - ww) / 25);
+  card5.style.setProperty('--mouseY5', (e.clientY - wh) / 25);
+
+});
+
+card5.addEventListener('mouseleave', function (e) {
+
+  card5.style.setProperty('--mouseX5', 0);
+  card5.style.setProperty('--mouseY5', 0);
+
+});
+/******************/
+var card6 = document.querySelector(".card--6");
+
+card6.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card6.style.setProperty('--mouseX6', (e.clientX - ww) / 25);
+  card6.style.setProperty('--mouseY6', (e.clientY - wh) / 25);
+
+});
+
+card6.addEventListener('mouseleave', function (e) {
+
+  card6.style.setProperty('--mouseX6', 0);
+  card6.style.setProperty('--mouseY6', 0);
+
+});
+/******************/
 
 /**************************  ANIMATION *****************************/
 
