a357d9e831e638f169fbd47473e4aba564ea86b5
victordalet <victordalet@protonmail.com>
2022-01-04 20:35:55 +0100

    my experiences

--- index/script.js
+++ index/script.js
@@ -79,6 +79,7 @@ images.forEach(image => {
   observer.observe(image)
 })
 
+
 /**************************  CARDS *****************************/
 var card1 = document.querySelector(".card--1");
 
@@ -188,6 +189,23 @@ card6.addEventListener('mouseleave', function (e) {
 
 });
 /******************/
+var card7 = document.querySelector(".card--7");
+
+card7.addEventListener('mousemove', function (e) {
+
+  var wh = window.innerHeight / 2,
+  ww = window.innerWidth / 2;
+  card7.style.setProperty('--mouseX7', (e.clientX - ww) / 25);
+  card7.style.setProperty('--mouseY7', (e.clientY - wh) / 25);
+
+});
+
+card7.addEventListener('mouseleave', function (e) {
+
+  card7.style.setProperty('--mouseX7', 0);
+  card7.style.setProperty('--mouseY7', 0);
+
+});
 
 
 
