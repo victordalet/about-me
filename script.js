7c56462f34543ac38d1b3955b6df5af8da020118
victordalet <victordalet@protonmail.com>
2022-09-08 12:10:59 +0200

    end new design

--- script.js
+++ script.js
@@ -136,3 +136,33 @@ function step() {
 
 init();
 step();
+
+/*************/
+let slidingNewsLetter = [];
+let nb = 19;
+var screenWidth = screen.width;
+let multiplicator;
+
+for (let nb_slide = 1 ; nb_slide < nb + 1; nb_slide++) {
+  slidingNewsLetter.push(document.querySelector('.slide'+nb_slide.toString()));
+}
+console.log(slidingNewsLetter);
+if (screenWidth < 1000){ multiplicator = 1.5;}
+else {multiplicator = 0.7;}
+console.log(multiplicator);
+
+window.addEventListener('scroll',()=>{
+  const {scrollTop,clientHeight} = document.documentElement;
+  let topElementToTopViewport = [];
+
+  for (let nb_slide = 0 ; nb_slide < nb ; nb_slide++) {
+    topElementToTopViewport.push(slidingNewsLetter[nb_slide].getBoundingClientRect().top);
+  }
+
+  for (let nb_slide = 0 ; nb_slide < nb ; nb_slide++) {
+    if(scrollTop > (scrollTop+topElementToTopViewport[nb_slide]).toFixed()-clientHeight*multiplicator){
+      slidingNewsLetter[nb_slide].classList.add('active'+(nb_slide+1).toString());
+    } 
+  }
+
+})
