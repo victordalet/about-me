e477ebf7f8106b1662cf11c5c1900127580642a3
unknown <victordalet@protonmail.com>
2022-11-04 17:16:45 +0100

    04 11

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 700, 'linear');
-  });
-});
-
 /************************************/
 
 
